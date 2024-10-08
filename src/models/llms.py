54e0503602b27aa1e6ca403b577a6b95bc938a77
quynguyen3490 <quynguyen3490@gmail.com>
2024-10-08 23:06:11 +0700

    fixed

--- src/models/llms.py
+++ src/models/llms.py
@@ -3,15 +3,12 @@ import os
 import streamlit as st
 
 def load_llm(model_name):
-    if os.getenv('OPENAI_API_KEY') is None:
-        OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
     
     if model_name == "gpt-3.5-turbo":
         return ChatOpenAI(
             model=model_name,
             temperature=0,
-            max_tokens=1000,
-            api_key=OPENAI_API_KEY
+            max_tokens=1000
         )
     else:
         raise ValueError(
--- src/utils.py
+++ src/utils.py
@@ -15,3 +15,25 @@ def execute_plt_code(code: str, df: pd.DataFrame):
     except Exception as e:
         st.error(f"Error executing plt code: {e}")
         return None
+    
+def execute_py_code(code: str, df: pd.DataFrame):
+
+    try:
+        local_vars = {"sns":sns, "plt":plt, "df":df}
+        complied_code = compile(code, "<string>", "exec")
+        exec(complied_code, globals(), local_vars)
+
+        st.code(code)
+
+        if 'output' in local_vars:
+            st.write("**Output:**")
+            st.write(local_vars['output'])
+        else:
+            st.write("No output dataframe.")
+
+        if 'chart' in local_vars:
+            st.pyplot(plt)
+        
+    except Exception as e:
+        st.error(f"Error executing python code: {e}")
+        return None
